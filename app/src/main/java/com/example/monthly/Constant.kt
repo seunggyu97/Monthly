0b68a05208eb8e40741f6f38689635edda378e53
seunggyu <seunggyu97@gmail.com>
2022-12-10 18:04:00 +0900

    [ADD] 앱 알림 테스트 기능 추가,

---
+++ app/src/main/java/com/example/monthly/Constant.kt
@@ -0,0 +1,9 @@
+package com.example.monthly
+
+class Constant {
+    companion object {
+        // 알림 아이디 선언
+        const val NOTIFICATION_ID = 0
+        const val CHANNEL_ID = "notification_channel"
+    }
+}
--- app/src/main/java/com/example/monthly/ui/SettingPushActivity.kt
+++ app/src/main/java/com/example/monthly/ui/SettingPushActivity.kt
@@ -1,14 +1,20 @@
 package com.example.monthly.ui
 
 import BottomSheetDialog
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.monthly.BasicActivity
+import com.example.monthly.Constant.Companion.NOTIFICATION_ID
 import com.example.monthly.GlobalApplication
+import com.example.monthly.MyReceiver
 import com.example.monthly.R
 import com.example.monthly.databinding.ActivitySettingPushBinding
 import com.example.monthly.enumClass.Status
@@ -22,6 +28,9 @@ class SettingPushActivity : BasicActivity(), InitDialogInterface {
     private lateinit var binding: ActivitySettingPushBinding
     private lateinit var settingPushViewModel: SettingPushViewModel
 
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
+
     private var isLableOn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +42,14 @@ class SettingPushActivity : BasicActivity(), InitDialogInterface {
     private fun init() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_setting_push)
         settingPushViewModel = ViewModelProvider(this).get(SettingPushViewModel::class.java)
+
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+        val intent = Intent(this, MyReceiver::class.java)
+        pendingIntent = PendingIntent.getBroadcast(
+            this, NOTIFICATION_ID, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
 
         // 앱 푸시 설정 상태 observe
         settingPushViewModel.pushStatus.observe(this) {
@@ -64,10 +81,8 @@ class SettingPushActivity : BasicActivity(), InitDialogInterface {
             btnTogglePush.setOnToggledListener(object : OnToggledListener {
                 override fun onSwitched(toggleableView: ToggleableView?, isOn: Boolean) {
                     if (isOn) {
-                        setLabelOn()
                         settingPushViewModel.setPushStatus(true)
                     } else {
-                        setLabelOff()
                         settingPushViewModel.setPushStatus(false)
                     }
                 }
@@ -77,6 +92,17 @@ class SettingPushActivity : BasicActivity(), InitDialogInterface {
                 if(isLableOn){
                     showBottomSheet()
                 }
+            }
+
+            // TODO : 앱 배포시 삭제(레이아웃 포함)
+            btnPushTest.setOnClickListener {
+                alarmManager.set(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    (SystemClock.elapsedRealtime() + 5000),
+                    pendingIntent
+                ) // set : 일회성 알림
+
+                Toast.makeText(application, "5초 후에 알림 울림", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -115,6 +141,9 @@ class SettingPushActivity : BasicActivity(), InitDialogInterface {
             btnTogglePush.isOn = false
             isLableOn = false
         }
+
+        // 알림 취소
+        alarmManager.cancel(pendingIntent)
     }
 
     fun showBottomSheet() {
