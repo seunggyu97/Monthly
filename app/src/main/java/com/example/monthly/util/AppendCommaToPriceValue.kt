807bb773aa2975b34fc83f73ebaa51dd9ee9d29a
seunggyu <seunggyu97@gmail.com>
2022-12-14 11:57:27 +0900

    [Update] 쉼표 구분 유틸 음수 버전 업데이트

--- app/src/main/java/com/example/monthly/util/AppendCommaToPriceValue.kt
+++ app/src/main/java/com/example/monthly/util/AppendCommaToPriceValue.kt
@@ -7,10 +7,18 @@ fun AppendCommaToPriceValue(resultInt: Int): String? {
     val resultStr = Integer.toString(resultInt)
     val length = resultStr.length
     // 7654321  ->  7,654,321
-    for (i in 0 until length) {
+
+    var start = 0
+    if(resultStr[0] == '-') start = 1
+
+    for (i in start until length) {
         sb.append(resultStr[i])
         if ((length - (i + 1)) % 3 == 0 && i != length - 1) sb.append(",")
     }
     Log.e("sb = ", sb.toString())
-    return sb.toString()
+    var answer = sb.toString()
+    if(start == 1) {
+        answer = "-$answer"
+    }
+    return answer
 }
